4bcf7c2d9c4e277baf8dc968a00795c452a07a4c
진세민|se min Jin <94339817+Dove-Dove@users.noreply.github.com>
2022-03-10 14:58:03 +0900

    2
    2

--- test 02.py
+++ test 02.py
@@ -1,16 +1,27 @@
-a = int(input())
-b = int(input())
-c = str(input())
-
+def add(a, b):
+    return a + b
 def minus(a, b):
-    result = a - b
-    print(result)
-    return result
+    return  a - b
 def muulti(a, b):
-    result = a * b
-    print(result)
-    return result
+    return  a * b
 def divide(a, b):
-    result = a / b
-    print(result)
-    return result
+    return  a / b
+
+
+while True:
+    a = input("첫번째 함수")
+    if a == "00000":
+        break
+    a= int(a)
+    b=int(input("두번째 함수"))
+    ad=add(a, b)
+    mi=minus(a, b)
+    mu=muulti(a, b)
+    di=divide(a, b)
+    print("add = ",ad)
+    print("minus = ",mi)
+    print("multi=",mi)
+    print("divide=",di)
+
+print("종료")
+    
