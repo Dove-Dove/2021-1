cf36d9d68781bc49192a599be9683cbc7931e13b
진세민|se min Jin <94339817+Dove-Dove@users.noreply.github.com>
2022-03-11 12:19:23 +0900

    java week 2
    4

---
+++ java/java0311.java
@@ -0,0 +1,16 @@
+
+public class java0311 {
+	
+public class DateType{
+	public static int seq(int n){
+		if(n == 1)
+			return 1;
+		else
+			return seq(n-1) +3;
+	}
+	 public static void main(String[] args) {
+		 for (int i=1; i<=10; i++) {
+			 System.out.print(seq(i)+" ");		 }
+	 }
+  }
+}
