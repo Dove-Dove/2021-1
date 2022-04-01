bb18736e1b69ccf6ce34eed4ba77ac2b80a4ea40
진세민|se min Jin <94339817+Dove-Dove@users.noreply.github.com>
2022-04-01 16:33:40 +0900

    Create Code05-2.py

---
+++ py/Code05-2.py
@@ -0,0 +1,55 @@
+#for i in range(0,10,1):
+
+#for i in range(7,-1,-1):
+ #   print(i)
+
+# l=["apple","banana","mango"]
+# l.append('melon')
+# print(len(l))
+#
+# print(l.pop())
+# print(l.pop())
+#
+# print(len(l))
+#
+# print(l[0])
+#
+# for i in l: #list
+#     print(i)
+
+# for x in range(2,9,2):
+#     for y in range(1,10):
+#         print(f'{x} * {y}= {x*y}')
+#     print("-------------0-0-0---")
+import random
+
+tries = 0
+guess= 0
+answer = random.randint(1,100)
+
+# print("1부터 100까지 숫자를 맞추시오")
+# while guess!=answer:
+#     guess = int(input("숫자 입력"))
+#     tries = tries + 1
+#     if tries == 10:
+#         break
+#
+#     if guess<answer:
+#         print("너무 낮음")
+#     elif guess>answer:
+#         print("너무 높음")
+#
+#
+#
+# if guess == answer:
+#     print(f'성공 시도횠수{tries}')
+# elif tries == 10:
+#     print('실패')
+# else:
+#     print(f'정답{answer}')
+
+
+for i in ['apple','banana','mango']:
+    print(i)
+
+
